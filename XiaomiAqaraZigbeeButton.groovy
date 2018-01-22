7c3c1c5731ef8b7077019d20ea365b6d32f967fa
castlecole <32769585+castlecole@users.noreply.github.com>
2018-01-22 00:50:39 +0000

    Update XiaomiAqaraZigbeeButton.groovy

--- XiaomiAqaraZigbeeButton.groovy
+++ XiaomiAqaraZigbeeButton.groovy
@@ -91,12 +91,12 @@ metadata {
         }
  
 	valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
-            state "default", label:'${currentValue}%', unit:"", icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/Battery.png",
+            state "default", label:'${currentValue}%'+"\n", unit:"", icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/Battery.png",
 			backgroundColors:[
 				[value: 0, color: "#c0392b"],
 				[value: 25, color: "#f1c40f"],
 				[value: 50, color: "#e67e22"],
-				[value: 75, color: "#27ae60"]
+				[value: 75, color: "#44b621"]
 			]
         }
         valueTile("lastcheckin", "device.lastCheckin", decoration: "flat", inactiveLabel: false, width: 4, height: 1) {
