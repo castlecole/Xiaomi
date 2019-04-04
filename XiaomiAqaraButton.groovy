5dd96e733a1c4a03761d1d48752a4c9cdfad54bd
castlecole <32769585+castlecole@users.noreply.github.com>
2019-04-04 10:00:18 +0100

    Update XiaomiAqaraButton.groovy

--- XiaomiAqaraButton.groovy
+++ XiaomiAqaraButton.groovy
@@ -41,7 +41,7 @@
  */
 
 def version() {
-	return "v2 (20170324)\nXiaomi Aqara Button - Zigbee"
+	return "v2.1 (20190404)\nXiaomi Aqara Button - Zigbee"
 }
 
 metadata {
@@ -78,8 +78,8 @@ metadata {
 
         multiAttributeTile(name:"button", type: "lighting", width: 6, height: 4, canChangeIcon: false) {
 		tileAttribute ("device.button", key: "PRIMARY_CONTROL") {
-			attributeState("pushed", label:'', icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/push-button-green-icon.png", backgroundColor:"#359148")
-			attributeState("released", label:'', icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/push-button-red-icon.png", backgroundColor:"#00a0dc")
+			attributeState("pushed", label:'', icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/ButtonOn.png", backgroundColor:"#359148")
+			attributeState("released", label:'', icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/ButtonOff.png", backgroundColor:"#00a0dc")
              	}
             	tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
                 	attributeState("default", label:'Last Update: ${currentValue}',icon: "st.Health & Wellness.health9")
@@ -88,8 +88,8 @@ metadata {
 
         multiAttributeTile(name:"button2", type: "lighting", width: 6, height: 4, canChangeIcon: false) {
 		tileAttribute ("device.button", key: "PRIMARY_CONTROL") {
-                	attributeState("pushed", label:'${name}', icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/push-button-green-icon.png", backgroundColor:"#359148")
-                	attributeState("released", label:'${name}', icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/push-button-red-icon.png", backgroundColor:"#00a0dc")
+                	attributeState("pushed", label:'${name}', icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/ButtonOn.png", backgroundColor:"#359148")
+                	attributeState("released", label:'${name}', icon:"https://raw.githubusercontent.com/castlecole/Xiaomi/master/ButtonOff.png", backgroundColor:"#00a0dc")
              	}
             	tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
                 	attributeState("default", label:'Last Update: ${currentValue}',icon: "st.Health & Wellness.health9")
