20c177a03d2891afb56d273cf5b2884b2967ef7d
castlecole <32769585+castlecole@users.noreply.github.com>
2019-12-18 23:28:15 +0000

    Create TuyaMotor.groovy

---
+++ TuyaMotor.groovy
@@ -0,0 +1,162 @@
+/**
+ *	Copyright 2015 SmartThings
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+def version() {
+	return "v1 (20191218)\nTuya Motor"
+}
+
+metadata {
+	definition (name: "Tuya Motor - ZigBee", namespace: "castlecole", author: "Sean Cole", ocfDeviceType: "oic.d.switch", runLocally: true, minHubCoreVersion: '000.019.00012', executeCommandsLocally: true, genericHandler: "Zigbee") {
+		capability "Actuator"
+		capability "Configuration"
+		capability "Refresh"
+		capability "Switch"
+		capability "Health Check"
+		attribute "lastCheckinDate", "Date"		
+		attribute "lastCheckin", "string"
+
+/*        fingerprint inClusters: "0000, 0003, 0004, 0005, 0006, 0008, 0300, 0B05, 1000", outClusters: "0005, 0019, 0020, 1000", manufacturer: "IKEA of Sweden",  model: "TRADFRI Button", deviceJoinName: "TRÅDFRI Button" 
+*/     
+}
+
+	// simulator metadata
+	simulator {
+		// status messages
+		status "on": "on/off: 1"
+		status "off": "on/off: 0"
+
+		// reply messages
+		reply "zcl on-off on": "on/off: 1"
+		reply "zcl on-off off": "on/off: 0"
+	}
+
+	preferences {
+		//Date & Time Config
+		input description: "", type: "paragraph", element: "paragraph", title: "DATE & CLOCK"    
+		input name: "dateformat", type: "enum", title: "Set Date Format\nUS (MDY) - UK (DMY) - Other (YMD)", description: "Date Format", options:["US","UK","Other"]
+		input name: "clockformat", type: "bool", title: "Use 24 hour clock?"
+		input description: "Version: ${version()}", type: "paragraph", element: "paragraph", title: ""
+	}
+
+	tiles(scale: 2) {
+		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00A0DC", nextState:"turningOff"
+				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
+				attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00A0DC", nextState:"turningOff"
+				attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
+			}
+			tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
+				attributeState("default", label:'${currentValue}', icon: "st.Health & Wellness.health9")
+			}
+		}
+		standardTile("power", "", inactiveLabel: True, decoration: "flat", width: 2, height: 2) {
+			state "default", icon:"https://raw.githubusercontent.com/castlecole/customdevices/master/Power-Icon.png"
+		}
+  	valueTile("blank", "", inactiveLabel: True, decoration: "flat", width: 2, height: 2) {
+        		state "default", label:''
+		}
+		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "default", label:"", action:"refresh.refresh", icon:"https://raw.githubusercontent.com/castlecole/customdevices/master/refresh.png"
+		}
+		main "switch"
+		details(["switch", "power", "blank", "refresh"])
+	}
+}
+
+// Parse incoming device messages to generate events
+def parse(String description) {
+	
+  // Determine current time and date in the user-selected date format and clock style
+	def now = formatDate()    
+	def nowDate = new Date(now).getTime()
+
+  log.debug "description is $description"
+	def event = zigbee.getEvent(description)
+        log.debug "parse(). Event : $event"
+        def parseVal = zigbee.parse(description)
+        log.debug "parse(). zigbee.parse : $parseVal" 
+
+  // Any report - test, smoke, clear in a lastCheckin event and update to Last Checkin tile
+	// However, only a non-parseable report results in lastCheckin being displayed in events log
+	sendEvent(name: "lastCheckin", value: now, displayed: true)
+	sendEvent(name: "lastCheckinDate", value: nowDate, displayed: false)
+
+if (event) {
+		sendEvent(event)
+	}
+	else {
+		log.warn "DID NOT PARSE MESSAGE for description : $description"
+		log.debug zigbee.parseDescriptionAsMap(description)
+	}
+}
+
+def off() {
+	zigbee.off()
+}
+
+def on() {
+	zigbee.on()
+}
+
+/**
+ * PING is used by Device-Watch in attempt to reach the Device
+ * */
+def ping() {
+	return refresh()
+}
+
+def refresh() {
+	zigbee.onOffRefresh() + zigbee.onOffConfig()
+}
+
+def configure() {
+	// Device-Watch allows 2 check-in misses from device + ping (plus 2 min lag time)
+	sendEvent(name: "checkInterval", value: 2 * 10 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+	log.debug "Configuring Reporting and Bindings."
+	zigbee.onOffRefresh() + zigbee.onOffConfig()
+}
+
+def formatDate(batteryReset) {
+	def correctedTimezone = ""
+	def timeString = clockformat ? "HH:mm:ss" : "h:mm:ss aa"
+
+	// If user's hub timezone is not set, display error messages in log and events log, and set timezone to GMT to avoid errors
+	if (!(location.timeZone)) {
+		correctedTimezone = TimeZone.getTimeZone("GMT")
+		log.error "${device.displayName}: Time Zone not set, so GMT was used. Please set up your location in the SmartThings mobile app."
+		sendEvent(name: "error", value: "", descriptionText: "ERROR: Time Zone not set, so GMT was used. Please set up your location in the SmartThings mobile app.")
+	} else {
+		correctedTimezone = location.timeZone
+	}
+	if (dateformat == "US" || dateformat == "" || dateformat == null) {
+		if (batteryReset){
+			return new Date().format("MMM dd yyyy", correctedTimezone)
+		} else {
+			return new Date().format("EEE MMM dd yyyy ${timeString}", correctedTimezone)
+		}
+	} else if (dateformat == "UK") {
+		if (batteryReset) {
+			return new Date().format("dd MMM yyyy", correctedTimezone)
+		} else {
+			return new Date().format("EEE dd MMM yyyy ${timeString}", correctedTimezone)
+		}
+	} else {
+		if (batteryReset) {
+			return new Date().format("yyyy MMM dd", correctedTimezone)
+		} else {
+			return new Date().format("EEE yyyy MMM dd ${timeString}", correctedTimezone)
+		}
+	}
+}
