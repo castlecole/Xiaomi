e6bd83e3114c310f7ea0953295255fe823367fd9
castlecole <32769585+castlecole@users.noreply.github.com>
2020-01-01 14:26:15 +0000

    Update ZigbeeKeyFobv1.groovy

--- ZigbeeKeyFobv1.groovy
+++ ZigbeeKeyFobv1.groovy
@@ -1,19 +1,43 @@
-metadata {
-definition (name: “Zigbee Key Fob V1”, namespace: “castlecole”, author: “Kevin Tierney”) {
+/**
+ *	Copyright 2019 Sean Cole
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
+ *
+ */
 
-	capability "Configuration"
-	capability "Refresh"
-	capability "Button"
-
-	attribute "button2","ENUM",["released","pressed"]
-	attribute "button3","ENUM",["released","pressed"]
-	attribute "numButtons", "STRING"
-
-	fingerprint profileId: "0104", deviceId: "0401", inClusters: "0000,0003,0500", outClusters: "0003,0501"
+def version() {
+	return "v1 (20191231)\nZigbee Key Fob"
 }
 
-tiles {
+metadata {
+	definition (name: "Key Fob - ZigBee", namespace: "castlecole", author: "Sean Cole", ocfDeviceType: "oic.d.switch", runLocally: true, minHubCoreVersion: '000.019.00012', executeCommandsLocally: true, genericHandler: "Zigbee") {
+		capability "Configuration"
+		capability "Refresh"
+		capability "Button"
 
+		attribute "button2","ENUM",["released","pressed"]
+		attribute "button3","ENUM",["released","pressed"]
+		attribute "numButtons", "STRING"
+
+		fingerprint profileId: "0104", deviceId: "0401", inClusters: "0000,0003,0500", outClusters: "0003,0501"
+	}
+
+	preferences {
+		//Date & Time Config
+		//input description: "", type: "paragraph", element: "paragraph", title: "DATE & CLOCK"    
+		//input name: "dateformat", type: "enum", title: "Set Date Format\nUS (MDY) - UK (DMY) - Other (YMD)", description: "Date Format", options:["US","UK","Other"]
+		//input name: "clockformat", type: "bool", title: "Use 24 hour clock?"
+		input description: "Version: ${version()}", type: "paragraph", element: "paragraph", title: ""
+	}
+
+	tiles(scale: 2) {
 	standardTile("button1", "device.button", width: 1, height: 1) {
 		state("released", label:'${name}', icon:"st.button.button.released", backgroundColor:"#ffa81e")
 		state("pressed", label:'${name}', icon:"st.button.button.pressed", backgroundColor:"#79b821")
@@ -31,70 +55,69 @@ tiles {
 
 	main (["button3", "button2", "button1"])
 	details (["button3", "button2", "button1"])
-}
+	}
 }
 
 def parse(String description) {
 
-if (description?.startsWith('enroll request')) {
-
-	List cmds = enrollResponse()
-	log.debug "enroll response: ${cmds}"
-	def result = cmds?.collect { new physicalgraph.device.HubAction(it) }
-	return result
-}
-else if (description?.startsWith('catchall:')) {
-	def msg = zigbee.parse(description)
-	log.debug msg
-	buttonPush(msg.data[0])
-}
-else {
-	log.debug "parse description: $description"
-}
+	if (description?.startsWith('enroll request')) {
+		List cmds = enrollResponse()
+		log.debug "enroll response: ${cmds}"
+		def result = cmds?.collect { new physicalgraph.device.HubAction(it) }
+		return result
+	}
+	else if (description?.startsWith('catchall:')) {
+		def msg = zigbee.parse(description)
+		log.debug msg
+		buttonPush(msg.data[0])
+	}
+	else {
+		log.debug "parse description: $description"
+	}
 }
 
 def buttonPush(button){
-def name = null
-log.debug “button number = $button”
-if (button == 0) {
-name = “1”
-def currentST = device.currentState(“button3”)?.value
-log.debug “Unlock button (3) Pushed”
-}
-else if (button == 2) {
-name = “2”
-def currentST = device.currentState(“button2”)?.value
-log.debug “Home button (2) pushed”
-}
-else if (button == 3) {
-name = “3”
-def currentST = device.currentState(“button”)?.value
-log.debug “Lock Button pushed”
-}
-def ButtonInt = name as Integer
-def result = createEvent(name: “button”, value: “pushed”, data: [buttonNumber: ButtonInt], descriptionText: “$device.displayName button name was pushed", isStateChange: true) log.debug "Parse returned {result?.descriptionText}”
-return result
+	def name = null
+	log.debug “button number = $button”
+	if (button == 0) {
+		name = “1”
+		def currentST = device.currentState(“button3”)?.value
+		log.debug “Unlock button (3) Pushed”
+	}
+	else if (button == 2) {
+		name = “2”
+		def currentST = device.currentState(“button2”)?.value
+		log.debug “Home button (2) pushed”
+	}
+	else if (button == 3) {
+		name = “3”
+		def currentST = device.currentState(“button”)?.value
+		log.debug “Lock Button pushed”
+	}
+	def ButtonInt = name as Integer
+	def result = createEvent(name: “button”, value: “pushed”, data: [buttonNumber: ButtonInt], descriptionText: “$device.displayName button name was pushed", isStateChange: true) log.debug "Parse returned {result?.descriptionText}”
+	return result
 }
 
 def enrollResponse() {
-log.debug “Sending enroll response”
-[
-“raw 0x500 {01 23 00 00 00}”, “delay 200”,
-“send 0x${device.deviceNetworkId} 0x08 1”
-]
-}
+	log.debug “Sending enroll response”
+	[
+	“raw 0x500 {01 23 00 00 00}”, “delay 200”,
+	“send 0x${device.deviceNetworkId} 0x08 1”
+	]
+	}
 
 def configure(){
-log.debug “Config Called”
+	log.debug “Config Called”
 
-// Set the number of buttons to 3
-sendEvent(name: "numButtons", value: "3", displayed: false)
+	// Set the number of buttons to 3
+	sendEvent(name: "numButtons", value: "3", displayed: false)
 
-def configCmds = [
-"zcl global write 0x500 0x10 0xf0 {${device.zigbeeId}}", "delay 200",
-"send 0x${device.deviceNetworkId} 0x08 1", "delay 1500",
-"zdo bind 0x${device.deviceNetworkId} 0x08 0x01 0x0501 {${device.zigbeeId}} {}", "delay 500",
-"zdo bind 0x${device.deviceNetworkId} 0x08 1 1 {${device.zigbeeId}} {}"
-]
-return configCmds
+	def configCmds = [
+	"zcl global write 0x500 0x10 0xf0 {${device.zigbeeId}}", "delay 200",
+	"send 0x${device.deviceNetworkId} 0x08 1", "delay 1500",
+	"zdo bind 0x${device.deviceNetworkId} 0x08 0x01 0x0501 {${device.zigbeeId}} {}", "delay 500",
+	"zdo bind 0x${device.deviceNetworkId} 0x08 1 1 {${device.zigbeeId}} {}"
+	]
+	return configCmds
 }
