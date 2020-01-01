b6264c145afe7af1bd74146a3b4789c3554adb06
castlecole <32769585+castlecole@users.noreply.github.com>
2020-01-01 14:41:11 +0000

    Update ZigbeeKeyFobv1.groovy

--- ZigbeeKeyFobv1.groovy
+++ ZigbeeKeyFobv1.groovy
@@ -76,39 +76,39 @@ def parse(String description) {
 	}
 }
 
-def buttonPush(button){
+def buttonPush(button) {
 	def name = null
-	log.debug “button number = $button”
+	log.debug "button number = $button"
 	if (button == 0) {
-		name = “1”
-		def currentST = device.currentState(“button3”)?.value
-		log.debug “Unlock button (3) Pushed”
+		name = "1"
+		def currentST = device.currentState("button3")?.value
+		log.debug "Unlock button (3) Pushed"
 	}
 	else if (button == 2) {
-		name = “2”
-		def currentST = device.currentState(“button2”)?.value
-		log.debug “Home button (2) pushed”
+		name = "2"
+		def currentST = device.currentState("button2")?.value
+		log.debug "Home button (2) pushed"
 	}
 	else if (button == 3) {
-		name = “3”
-		def currentST = device.currentState(“button”)?.value
-		log.debug “Lock Button pushed”
+		name = "3"
+		def currentST = device.currentState("button")?.value
+		log.debug "Lock Button pushed"
 	}
 	def ButtonInt = name as Integer
-	def result = createEvent(name: “button”, value: “pushed”, data: [buttonNumber: ButtonInt], descriptionText: “$device.displayName button name was pushed", isStateChange: true) log.debug "Parse returned {result?.descriptionText}”
+	def result = createEvent(name: "button", value: "pushed", data: [buttonNumber: ButtonInt], descriptionText: "$device.displayName button name was pushed", isStateChange: true) log.debug "Parse returned {result?.descriptionText}"
 	return result
 }
 
 def enrollResponse() {
-	log.debug “Sending enroll response”
+	log.debug "Sending enroll response"
 	[
-	“raw 0x500 {01 23 00 00 00}”, “delay 200”,
-	“send 0x${device.deviceNetworkId} 0x08 1”
+	"raw 0x500 {01 23 00 00 00}", "delay 200",
+	"send 0x${device.deviceNetworkId} 0x08 1"
 	]
-	}
+}
 
 def configure(){
-	log.debug “Config Called”
+	log.debug "Config Called"
 
 	// Set the number of buttons to 3
 	sendEvent(name: "numButtons", value: "3", displayed: false)
